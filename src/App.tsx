9b4da3a77016587dc9639f53bb51eebac4463c4b
CodeGod36 <jayjaybowofola@gmail.com>
2024-03-24 12:26:38 +0100

    SignUp Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-const App = () => {
-  return <div className="App"></div>;
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/DropDown.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function DropDown() {
+  const [selectOption, setSelectOption] = useState("");
+  const handleChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setSelectOption(event.target.value);
+  };
+  return (
+    <div>
+      <select
+        id="dropDown"
+        value={selectOption}
+        onChange={handleChange}
+        className="w-[17rem] my-4 h-[2.5rem] px-1 rounded-[0.7rem] border-solid border-2 border-[#000000]"
+      >
+        <option value="">Select...</option>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+    </div>
+  );
+}
+
+export default DropDown;
