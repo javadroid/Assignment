b604121c54f4568cd93e9df5439f2948e1190016
CodeLord36 <93430616+CodeLord36@users.noreply.github.com>
2024-03-25 02:19:36 +0100

    Edited the signin and ForgetPassword

--- src/components/ForgetPassword.tsx
+++ src/components/ForgetPassword.tsx
@@ -5,54 +5,59 @@ import { Link } from "react-router-dom";
 
 const ForgetPassword = () => {
   return (
-    <div className="w-full">
+    <div className="w-full font-pop">
       <div className="flex flex-row text-gray-900">
         <div className="image basis-1/3"></div>
         <div className="basis-2/3 flex flex-col pt-28 px-24 pb-96">
           <div className="flex flex-row justify-between mb-28 items-center">
-            <Link to={"/signIn"} className="color">
+            <Link to={"/signIn"} className="text-[#a1812e]">
               <FaArrowLeft />
             </Link>
             <p className="font-medium">
               Don't have an account?{" "}
-              <Link to={"/"} className="color font-semibold">
+              <Link to={"/"} className="text-[#a1812e] font-semibold">
                 Sign Up
               </Link>
             </p>
           </div>
           <div>
-            <h1 className="font-bold text-gray-700 text-6xl pb-4">Sign In</h1>
+            <h1 className="font-bold text-gray-700 text-6xl pb-4">
+              Reset Password
+            </h1>
             <p className="text-gray-500 text-base mb-16">
-              Fill in your details to sign in
+              Input your email address to retrieve your password
             </p>
             <form action="">
               <div>
                 <div className="mb-10">
                   <label
-                    htmlFor="userId"
+                    htmlFor="email"
                     className="block text-lg font-medium leading-6 text-gray-500"
                   >
-                    User ID:
+                    Email Address:
                   </label>
                   <div className="">
                     <input
-                      type="text"
-                      className="w-72 my-4 h-10 px-2 rounded-xl pr-10 focus:w-80 focus"
+                      type="email"
+                      className="w-72 my-4 h-10 px-2 rounded-xl pr-10 border-2 border-gray-500 focus:w-80 focus:border-[#a1812e]"
                     />
                   </div>
                 </div>
 
                 <div className="mb-16">
-                  <button className="button w-72 flex flex-row justify-center items-center px-16 py-2 rounded-xl">
-                    <span className="text-base">Sign In</span>
+                  <button className="button w-72 flex flex-row justify-center items-center px-16 py-2 rounded-xl bg-[#a1812e]">
+                    <span className="text-base text-white">Proceed</span>
                   </button>
                 </div>
 
                 <div className="">
                   <p className="font-medium">
-                    Forget your password?{" "}
-                    <Link to={"/forget"} className="color font-semibold">
-                      Reset Now!
+                    Remember your password?{" "}
+                    <Link
+                      to={"/signIn"}
+                      className="font-semibold text-[#a1812e]"
+                    >
+                      Sign In!
                     </Link>
                   </p>
                 </div>
