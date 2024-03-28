09b249ea3b4c84cf7e4990e97a8480f1f20b4dc7
CodeGod36 <jayjaybowofola@gmail.com>
2024-03-28 10:46:35 +0100

    updateInput

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,18 +4,14 @@ interface Props {
   labelText: string;
   id: string;
   type: string;
+  className: string;
 }
 
-const InputField: React.FC<Props> = ({ labelText, id, type }) => {
+const InputField: React.FC<Props> = ({ labelText, id, type, className }) => {
   return (
     <div className="flex flex-col">
       <label htmlFor={id}>{labelText}</label>
-      <input
-        type={type}
-        id={id}
-        className="borderBlack border-2 w-[17rem] mb-1 h-[2.5rem] px-2 rounded-[0.7rem]
-         outline-none  focus:border-[#a1812e]"
-      />
+      <input type={type} id={id} className={className} />
     </div>
   );
 };
