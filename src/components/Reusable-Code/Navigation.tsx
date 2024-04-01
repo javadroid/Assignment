295b13697a9e3e57a68d36c203c073f764e688c9
CodeGod36 <jayjaybowofola@gmail.com>
2024-04-01 20:44:18 +0100

    navIcon

--- src/components/Reusable-Code/Navigation.tsx
+++ src/components/Reusable-Code/Navigation.tsx
@@ -1,9 +1,14 @@
 import { FaBars, FaPowerOff } from "react-icons/fa6";
+import { MdCircle } from "react-icons/md";
 
 function Navigation() {
   return (
     <nav className="p-[1rem] w-full flex flex-row justify-between border-b-2 border-b-gray-300 shadow-md">
-      <FaBars size={20} />
+      <div className="relative">
+        <FaBars size={20} />
+        <MdCircle size={9} className="absolute right-0 top-0" />
+      </div>
+
       <FaPowerOff size={20} style={{ color: "#ef1616" }} />
     </nav>
   );
