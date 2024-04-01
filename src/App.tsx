b38d3f8d27c4d89ac7332c6bf8477cb863dbe175
CodeGod36 <jayjaybowofola@gmail.com>
2024-04-01 19:14:32 +0100

    added Page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ForgetPassword from "./components/ForgetPassword";
 import ResetPassword from "./components/ResetPassword";
 import NewPassword from "./components/NewPassword";
+import UploadedProject from "./components/Student-Dashboard/UploadedProject";
 
 const App: React.FC = () => {
   return (
@@ -14,6 +15,7 @@ const App: React.FC = () => {
         <Route path="/forget" element={<ForgetPassword />}></Route>
         <Route path="/reset" element={<ResetPassword />}></Route>
         <Route path="/newpassword" element={<NewPassword />}></Route>
+        <Route path="/uploadedProject" element={<UploadedProject />}></Route>
       </Routes>
     </Router>
   );
---
+++ src/components/Student-Dashboard/UploadedProject.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UploadedProject() {
+  return (
+    <div>
+      <div className="h-screen w-5 bg-[#57430e]"></div>
+    </div>
+  );
+}
+
+export default UploadedProject;
