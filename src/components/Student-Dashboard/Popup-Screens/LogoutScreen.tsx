9d42a2d754c56cb0edcfe6019ef6f95dba44764d
CodeGod36 <jayjaybowofola@gmail.com>
2024-04-05 01:34:38 +0100

    phase one of the screen updated completely

--- src/components/Student-Dashboard/Popup-Screens/LogoutScreen.tsx
+++ src/components/Student-Dashboard/Popup-Screens/LogoutScreen.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { IoClose } from "react-icons/io5";
+import { IoMdCheckmark } from "react-icons/io";
+import { Link } from "react-router-dom";
 interface LogoutScreenProps {
   onClose: () => void;
 }
@@ -9,13 +11,20 @@ const LogoutScreen = ({ onClose }: LogoutScreenProps) => {
     <div className="font-pop fixed top-0 left-0 w-full h-full bg-black bg-opacity-50   flex justify-center items-center z-10">
       <div className="bg-slate-50 p-[2rem] rounded-md z-[7rem] w-[30rem]  shadow-lg">
         <div className="flex flex-col">
-          <caption>Hello</caption>
-          <IoClose
-            onClick={onClose}
-            style={{ cursor: "pointer" }}
-            size={25}
-            className=""
-          />
+          <div className="my-2">
+            <h3 className="flex flex-col mb-2">Logout Now</h3>
+            <caption className="flex justify-center">Confirm Process:</caption>
+          </div>
+          <div className="mx-[5rem] flex flex-row justify-between">
+            <IoClose
+              onClick={onClose}
+              style={{ cursor: "pointer", fill: "#e11d48" }}
+              size={35}
+            />
+            <Link to={"/"}>
+              <IoMdCheckmark size={35} style={{ fill: "#a1812e" }} />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
