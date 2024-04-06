8be53e9dc7e878b4ca868bc3f33d8d744a2036c7
CodeLord36 <93430616+CodeLord36@users.noreply.github.com>
2024-04-06 18:40:15 +0100

    updated dashboard screen

--- src/components/Student-Dashboard/Dashboard.tsx
+++ src/components/Student-Dashboard/Dashboard.tsx
@@ -4,30 +4,42 @@ import Navigation from "../Reusable-Code/Navigation";
 
 const Dashboard = () => {
   return (
-    <div className="h-screen flex flex-row">
+    <div className="component flex flex-row">
       <SideDesign />
       <div className="w-full">
         <Navigation />
-        <div className="">
-          <section>
-            <h1>Project Proposal</h1>
-            <hr />
-            <div>
-              <button>Ongoing</button>
+        <div className="pt-48 px-14 flex flex-row justify-between">
+          <section className="basis-1/3 m-4 bg-gradient-to-r from-[#EDBE44] from-1% to-[#8D7F5B] to-100%">
+            <h1 className="text-black font-medium text-2xl py-2 text-center">
+              Project Proposal
+            </h1>
+            <hr className="border-black" />
+            <div className="p-12 flex justify-center">
+              <button className="bg-white rounded-md px-14 py-2 uppercase text-[#A1812E] font-bold text-lg">
+                Ongoing
+              </button>
             </div>
           </section>
-          <section>
-            <h1>Internal Defense</h1>
-            <hr />
-            <div>
-              <button>Inactive</button>
+          <section className="basis-1/3 m-4 bg-gradient-to-b from-[#F9E8BC] from-1% to-[#999999] to-100%">
+            <h1 className="text-black font-medium text-2xl py-2 text-center">
+              Internal Defense
+            </h1>
+            <hr className="border-black" />
+            <div className="p-12 flex justify-center">
+              <button className="bg-white rounded-md px-14 py-2 uppercase text-[#A1812E] font-bold text-lg">
+                Inactive
+              </button>
             </div>
           </section>
-          <section>
-            <h1>External Defense</h1>
-            <hr />
-            <div>
-              <button>Inactive</button>
+          <section className="basis-1/3 m-4 bg-gradient-to-l from-[#A89D82] from-1% to-[#F3D380] to-100%">
+            <h1 className="text-black font-medium text-2xl py-2 text-center">
+              External Defense
+            </h1>
+            <hr className="border-black" />
+            <div className="p-12 flex justify-center">
+              <button className="bg-white rounded-md px-14 py-2 uppercase text-[#A1812E] font-bold text-lg">
+                Inactive
+              </button>
             </div>
           </section>
         </div>
@@ -35,5 +47,9 @@ const Dashboard = () => {
     </div>
   );
 };
+
+// for box 1 #EDBE44 #8D7F5B
+// for box 2 #F9E8BC #999999
+// for box 3 #A89D82 #F3D380
 
 export default Dashboard;
