56649fa49752e551e058da87d47bca838d251d14
CodeGod36 <jayjaybowofola@gmail.com>
2024-06-05 20:34:55 +0100

    signIn...

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Button from "./Reusable-Code/Button";
 import InputField from "./Reusable-Code/InputField";
@@ -10,13 +9,13 @@ function SignIn() {
   // }, []); // Empty dependency array to ensure it only runs once
 
   return (
-    <div className="font-pop text-gray-500">
+    <div className="lg:overflow-hidden font-pop lg:h-screen text-gray-500">
       {/* Container */}
       <div className="flex flex-wrap lg:flex-nowrap justify-center lg:justify-between lg:w-full text-gray-900">
         {/* Side Image */}
-        <div className="image lg:w-1/2 xl:w-2/3 hidden lg:block h-screen bg-cover bg-center"></div>
+        <div className="image lg:w-1/2 xl:w-2/3 hidden lg:block h-screen bg-cover bg-center lg:overflow-hidden"></div>
         {/* The Signup page */}
-        <div className="lg:w-1/2 xl:w-1/3 p-10 lg:p-20 flex flex-col justify-center">
+        <div className="lg:w-1/2 xl:w-1/3 p-10 lg:p-20 flex flex-col justify-center ">
           <div className="flex justify-end mb-4">
             <caption className="text-sm">
               Don't have an account?{" "}
