161dba0dcd6c5c62f74504bae65df5f047e26483
CodeGod36 <jayjaybowofola@gmail.com>
2024-08-02 22:41:34 +0100

    restructured signup

--- src/components/Reusable-Code/DropDown.tsx
+++ src/components/Reusable-Code/DropDown.tsx
@@ -1,94 +1,45 @@
-import React from "react";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import Box from "@mui/material/Box";
-import MenuItem from "@mui/material/MenuItem";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import React, { useState } from "react";
 import { lecturersListDataATH } from "../../Utilities/Data";
-import { Theme, useTheme } from "@mui/material/styles";
-import Chip from "@mui/material/Chip";
 
 interface Props {
-  label: string;
+  id?: string;
   className?: string;
+  labelText: string;
+  divClassName?: string;
 }
 
-function getStyles(name: string, personName: readonly string[], theme: Theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
+const DropDown: React.FC<Props> = ({
+  id,
+  className,
+  labelText,
+  divClassName,
+}) => {
+  const [selectOption, setSelectOption] = useState("");
+  const handleChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setSelectOption(event.target.value);
   };
-}
-
-const DropDownMenu: React.FC<Props> = ({ label, className }) => {
-  const theme = useTheme();
-  const [personName, setPersonName] = React.useState<string[]>([]);
-
-  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
-    const {
-      target: { value },
-    } = event;
-    setPersonName(
-      // On autofill we get a stringified value.
-      typeof value === "string" ? value.split(",") : value
-    );
-  };
-
   return (
-    <div>
-      <Box sx={{ minWidth: 120, mb: 2 }}>
-        <Select
-          // sx={{ backgroundColor: "#f6dd9e" }}
-          className="w-[100%]"
-          displayEmpty
-          value={personName}
-          onChange={handleChange}
-          input={<OutlinedInput />}
-          renderValue={(selected) => {
-            if (selected.length === 0) {
-              return <em>Select from Lecturer</em>;
-            }
-
-            return selected.join(", ");
-          }}
-        >
-          <MenuItem disabled value="">
-            <em>Faculty</em>
-          </MenuItem>
-          {lecturersListDataATH.map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, personName, theme)}
-            >
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
-      </Box>
-      {/* display the selected lecturer */}
-      <div className="mt-10 w-[100%] font-medium ">
-        {personName.length > 0 && (
-          <div>
-            <h3 className={className}>{label}:</h3>
-            <Box
-              sx={{
-                display: "flex",
-                flexWrap: "wrap",
-                gap: 0.5,
-                marginTop: 1.5,
-              }}
-            >
-              {personName.map((name) => (
-                <Chip key={name} label={name} />
-              ))}
-            </Box>
-          </div>
-        )}
-      </div>
+    <div className={divClassName}>
+      <label htmlFor="">{labelText}</label>
+      <select
+        id={id}
+        value={selectOption}
+        onChange={handleChange}
+        className={className}
+      >
+        <option value="" disabled>
+          <em>Select...</em>
+        </option>
+        {lecturersListDataATH.map((name) => (
+          <option key={name} value={name}>
+            {name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
 
-export default DropDownMenu;
+export default DropDown;
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -8,7 +8,7 @@ import ShowPassword from "./Reusable-Code/ShowPassword";
 
 const SignUp: React.FC = () => {
   return (
-    <div className="overflow-auto lg:overflow-hidden font-pop h-screen text-gray-500">
+    <div className="overflow-auto lg:overflow-auto font-pop h-screen text-gray-500">
       {/* Sign up header */}
       <div className="flex justify-between xs:mx-[1.5rem] lg:mx-[15rem] my-3">
         <div className="">
@@ -85,10 +85,71 @@ const SignUp: React.FC = () => {
               outline-none  focus:border-[#a1812e]"
               />
               <DropDown
-                label="Faculty:"
+                divClassName="flex flex-col"
+                labelText="Faculty:"
+                id="dropDown"
                 className="borderBlack border-2 xs:w-[12rem] lg:w-[17rem] mb-1 h-[2.5rem] px-2 rounded-[0.7rem]
               outline-none  focus:border-[#a1812e]"
               />
+            </div>
+            <div className="flex justify-between">
+              <DropDown
+                divClassName="flex flex-col"
+                labelText="Department:"
+                id="dropDown"
+                className="borderBlack border-2 xs:w-[12rem] lg:w-[17rem] mb-1 h-[2.5rem] px-2 rounded-[0.7rem]
+              outline-none  focus:border-[#a1812e]"
+              />
+              <DropDown
+                divClassName="flex flex-col"
+                labelText="Role:"
+                id="dropDown"
+                className="borderBlack border-2 xs:w-[12rem] lg:w-[17rem] mb-1 h-[2.5rem] px-2 rounded-[0.7rem]
+              outline-none  focus:border-[#a1812e]"
+              />
+            </div>
+            <DropDown
+              divClassName="flex flex-col"
+              labelText="Type:"
+              id="dropDown"
+              className="borderBlack border-2 xs:w-[12rem] lg:w-[17rem] mb-1 h-[2.5rem] px-2 rounded-[0.7rem]
+              outline-none  focus:border-[#a1812e]"
+            />
+            <div className="flex justify-between">
+              <ShowPassword
+                labelText="Password:"
+                id="confirmPassword"
+                className="borderBlack border-2 xs:w-[12rem] lg:w-[17rem] mb-1 h-[2.5rem] px-2 rounded-[0.7rem]
+              outline-none  focus:border-[#a1812e]"
+              />
+              <ShowPassword
+                labelText="Confirm Password:"
+                id="password"
+                className="borderBlack border-2 xs:w-[12rem] lg:w-[17rem] mb-1 h-[2.5rem] px-2 rounded-[0.7rem]
+              outline-none  focus:border-[#a1812e]"
+              />
+            </div>
+            {/* submit btn */}
+            <div className="my-5">
+              <p className="lg:text-sm text-wrap w-full mt-2">
+                By clicking continue, I agree to{" "}
+                <Link className="text-[#a1812e]" to="/">
+                  Terms of Use
+                </Link>{" "}
+                and acknoledge that I have read the{" "}
+                <Link className="text-[#a1812e]" to="/">
+                  Privacy Policy
+                </Link>
+              </p>
+              <div className="mt-3 flex justify-center">
+                <button
+                  type="submit"
+                  className="group lg:mt-2 w-[50rem] h-[3rem] flex flex-row justify-center items-center px-16 py-2 rounded-xl bg-[#a1812e]"
+                >
+                  <span className="text-base text-white">Sign Up</span>
+                  <GoArrowRight className="ml-2 text-xl text-white opacity-70 group-hover:translate-x-1 transition" />
+                </button>
+              </div>
             </div>
           </div>
         </form>
