b70e887332a8604599097749aff3236bd30fc6e2
CodeGod36 <jayjaybowofola@gmail.com>
2024-08-04 13:08:36 +0100

    Sidebar Modified

--- src/components/Reusable-Code/SideDesign.tsx
+++ src/components/Reusable-Code/SideDesign.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const SideDesign = () => {
   return (
-    <div className="h-screen bg-[#57430e] xs:w-[17%]  transition-transform ease-in-out">
+    <div className="h-screen bg-[#57430e] xs:w-[17%]  transition-transform ease-in-out text-slate-100 xs:text-[10px] lg:text-[20px]">
       <ul className="flex flex-col text-center gap-y-28 h-full justify-center ">
-        <li className="group flex  rounded-md h-[4rem] cursor-pointer hover:bg-[#f6dd9e]">
+        <li className="group flex  rounded-md h-[4rem] cursor-pointer hover:bg-[#f6dd9e] ">
           <Link
-            className="group-hover:text-gray-950 group-hover:scale-110 transition-transform ease-linear  flex flex-col text-center m-auto"
+            className="group-hover:text-gray-950  group-hover:scale-110 transition-transform ease-linear  flex flex-col text-center m-auto"
             to={"/dash"}
           >
             Dashboard
