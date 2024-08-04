672b0b81b257b0dad2dbac3bbd5c8390d9adf04a
CodeGod36 <jayjaybowofola@gmail.com>
2024-08-04 13:03:16 +0100

    sidebar implementation

--- src/components/Reusable-Code/SideDesign.tsx
+++ src/components/Reusable-Code/SideDesign.tsx
@@ -1,7 +1,44 @@
-function SideDesign() {
+import { Link } from "react-router-dom";
+
+const SideDesign = () => {
   return (
-    <div className="h-screen bg-[#57430e] xs:w-[15%]  transition-transform ease-in-out"></div>
+    <div className="h-screen bg-[#57430e] xs:w-[17%]  transition-transform ease-in-out">
+      <ul className="flex flex-col text-center gap-y-28 h-full justify-center ">
+        <li className="group flex  rounded-md h-[4rem] cursor-pointer hover:bg-[#f6dd9e]">
+          <Link
+            className="group-hover:text-gray-950 group-hover:scale-110 transition-transform ease-linear  flex flex-col text-center m-auto"
+            to={"/dash"}
+          >
+            Dashboard
+          </Link>
+        </li>
+        <li className="group flex  rounded-md h-[4rem] cursor-pointer hover:bg-[#f6dd9e]">
+          <Link
+            className="group-hover:text-gray-950 group-hover:scale-110 transition-transform ease-linear  flex flex-col text-center m-auto"
+            to={"/proposal"}
+          >
+            Proposal
+          </Link>
+        </li>
+        <li className="group flex  rounded-md h-[4rem] cursor-pointer hover:bg-[#f6dd9e]">
+          <Link
+            className="group-hover:text-gray-950 group-hover:scale-110 transition-transform ease-linear  flex flex-col text-center m-auto"
+            to={"/dash"}
+          >
+            Dashboard
+          </Link>
+        </li>
+        <li className="group flex  rounded-md h-[4rem] cursor-pointer hover:bg-[#f6dd9e]">
+          <Link
+            className="group-hover:text-gray-950 group-hover:scale-110 transition-transform ease-linear  flex flex-col text-center m-auto"
+            to={"/dash"}
+          >
+            Dashboard
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
-}
+};
 
 export default SideDesign;
